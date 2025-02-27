9dd903a6ce0c7611b95765aa1ba0406405ea5265
Abdcode137 <abdulrehman.qamer123@gmail.com>
2025-02-27 19:20:03 +0500

    Add files via upload

---
+++ PatternsA.C.py
@@ -0,0 +1,8 @@
+print("Mirrored Right-Angle Of Stars (*):") 
+n = int(input("Enter The Number Of Rows: "))
+for i in range(n):
+    for j in range(n - i - 1): 
+        print(" ", end=" ")
+    for j in range(i + 1):  
+        print("*", end=" ")
+    print()
